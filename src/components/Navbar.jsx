c30f7b832ab6c48e0255303979798ac34b555c7a
Kyung <kyung.mi.kang@gritacademy.se>
2024-01-25 13:01:24 +0100

    25-1

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,20 +2,21 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { FaShopify } from "react-icons/fa";
 import { CiEdit } from "react-icons/ci";
+import { FaShoppingCart } from "react-icons/fa";
 
 export default function Navbar(){
 
     return(
-        <header>
-            <Link to ='/'>
+        <header className='flex justify-between border-b border-gray-300 p-5'>
+            <Link to ='/' className='flex items-center text-4xl text-brand'>
                 <FaShopify />
                 <h1>Shoppy</h1>
             </Link>
 
-            <nav>
+            <nav className='flex items-center gap-4 font-semibold'>
                 <Link to = '/products'>Products</Link>
-                <Link to = '/carts'>Carts</Link>
-                <Link to= '/products/new'>
+                <Link to = '/carts' className='text-brand'><FaShoppingCart /></Link>
+                <Link to= '/products/new' className='text-2xl text-brand'>
                     <CiEdit/>
                 </Link>
                 <button>Log In</button>
