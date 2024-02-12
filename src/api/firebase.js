86a252c008cc5516c89955e23eb53726ede393b6
Kyung <kyung.mi.kang@gritacademy.se>
2024-02-12 20:55:26 +0100

    2-12-2

--- src/api/firebase.js
+++ src/api/firebase.js
@@ -1,16 +1,19 @@
-
-import { initializeApp } from "firebase/app";
-import {v4 as uuid} from 'uuid';
-import { getAuth, signInWithPopup, signOut, onAuthStateChanged, GoogleAuthProvider } from "firebase/auth";
-import { getDatabase, ref, set, get } from "firebase/database";
-
+import { initializeApp } from 'firebase/app';
+import { v4 as uuid } from 'uuid';
+import {
+  getAuth,
+  signInWithPopup,
+  GoogleAuthProvider,
+  signOut,
+  onAuthStateChanged,
+} from 'firebase/auth';
+import { getDatabase, ref, set, get } from 'firebase/database';
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL:process.env.REACT_APP_FIREBASE_DB_URL,
+  databaseURL: process.env.REACT_APP_FIREBASE_DB_URL,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
- 
 };
 
 const app = initializeApp(firebaseConfig);
@@ -18,62 +21,49 @@ const auth = getAuth();
 const provider = new GoogleAuthProvider();
 const database = getDatabase(app);
 
-//log In
-export function login(){
-signInWithPopup(auth, provider).catch(error=> console.error(error)
-    );
+export function login() {
+  signInWithPopup(auth, provider).catch(console.error);
 }
 
-//Log Out
-export function logout(){
- signOut(auth).catch(error=> console.error(error)
-    );
+export function logout() {
+  signOut(auth).catch(console.error);
 }
 
-//Listen to state changes
-export function onUserStateChange ( callback ){
-onAuthStateChanged(auth, async (user)=>{
-
-  const updatedUser = user ? await adminUser(user) : null;
-  callback(updatedUser)}
-  );
-}
-
-//Auth 
-export async function adminUser(user){
-
-  return get(ref(database,'admins'))//
-
-        .then((snapshot)=>{
-    if(snapshot.exists()){
-      const admins = snapshot.val();
-      const isAdmin = admins.includes(user.uid); //check if admin is already in the database. isAdmin: true or false??
-      return { ...user, isAdmin}
-    }
-      return user;
+export function onUserStateChange(callback) {
+  onAuthStateChanged(auth, async (user) => {
+    const updatedUser = user ? await adminUser(user) : null;
+    callback(updatedUser);
   });
 }
 
-// Add product
-export async function addNewProduct(product, url){
-  const id = uuid();
-set (ref(database, `products/${id}`),{
-  ...product, // receive all keys and values from curent product
-  id, 
-  imageUrl:url,
-  price: parseInt(product.price), // store info as nimber price
-  
-});
+async function adminUser(user) {
+  return get(ref(database, 'admins')) //
+    .then((snapshot) => {
+      if (snapshot.exists()) {
+        const admins = snapshot.val();
+        const isAdmin = admins.includes(user.uid);
+        return { ...user, isAdmin };
+      }
+      return user;
+    });
 }
 
-export async function getProducts(){
+export async function addNewProduct(product, imageUrl) {
+  const id = uuid();
+  return set(ref(database, `products/${id}`), {
+    ...product,
+    id,
+    price: parseInt(product.price),
+    imageUrl,
+    options: product.options.split(','),
+  });
+}
 
-  return get(ref(database, 'products')).then(snapshot=>{
-
-    if(snapshot.exists()){
-return Object.values(snapshot.val());
+export async function getProducts() {
+  return get(ref(database, 'products')).then((snapshot) => {
+    if (snapshot.exists()) {
+      return Object.values(snapshot.val());
     }
-  return [];
-})
-  }
-
+    return [];
+  });
+}
--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 //import Products from './Products';
+import { useNavigate } from 'react-router-dom';
 
-export default function ProductCard({product:{id, imageUrl, title, description, price} }) {
-    return<li className='rounded-1xl shadow-md overflow-hidden cursor-pointer'>
+export default function ProductCard(
+    
+    {product,
+     product:{id, imageUrl, title, description, price} }) {
+    
+    const navigate = useNavigate();
+return<li onClick={()=>{
+    navigate(`/products/${id}`,{state: {product}})
+}}
+            className='rounded-1xl shadow-md overflow-hidden cursor-pointer transition-all hover:scale-105'>
         <img className='w-full' src={imageUrl} alt={title}/>
-        <div className='mt-2 px-2 text-lg flex justify-around'>
+        <div className='mt-2 px-2 text-lg flex justify-evenly'>
             <h3 className='truncate'>{title}</h3>
             <h3>{`WON:${price}`}</h3>
         </div>
